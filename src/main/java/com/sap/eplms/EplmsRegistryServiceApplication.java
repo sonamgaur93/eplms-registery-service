826dcfe6df06f712f476aa377f0f0ae3025ba936
sonam <sonam.gour@aartek.in>
2023-08-19 13:42:22 +0530

    refector class name as per the naming convention

--- src/main/java/com/sap/eplms/EplmsRegistryServiceApplication.java
+++ src/main/java/com/sap/eplms/EplmsRegistryServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EServerApplication {
+public class EplmsRegistryServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EServerApplication.class, args);
+		SpringApplication.run(EplmsRegistryServiceApplication.class, args);
 	}
 
 }
